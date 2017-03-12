fe2dd649e933d8e152f8db38d7bea56dc68be0a1
Tenorio Orta JM <ente011@gmailcom>
2017-03-12 14:15:52 -0600

    feat: Add basic projects CRUD

--- api/workflow/urls.py
+++ api/workflow/urls.py
@@ -1,7 +1,11 @@
+
 from django.conf.urls import url
 from django.contrib import admin
 
+from workflow.views.project import ProjectList, ProjectDetail
+
 
 urlpatterns = [
-    # url(r'^admin/', admin.site.urls),
+    url(r'^projects/$', ProjectList.as_view()),
+    url(r'^projects/(?P<pk>[0-9]+)/$', ProjectDetail.as_view()),
 ]
