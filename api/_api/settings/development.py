45cd0f2a6cd5888bff1ca72e75e3e856d9baa38d
Tenorio Orta JM <ente011@gmailcom>
2017-03-14 22:32:07 -0600

    feat: Add and configurate django-cors-headers

--- api/_api/settings/development.py
+++ api/_api/settings/development.py
@@ -29,16 +29,57 @@ DATABASES = {
 INSTALLED_APPS += [
     'django_extensions',
     'debug_toolbar',
+    'corsheaders',
 ]
 
 MIDDLEWARE += [
+    # debug-toolbar
     'debug_toolbar.middleware.DebugToolbarMiddleware',
+
+    # django-cors-headers
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
+# ***** DEBUG-TOOLBAR SETTINGS ***** #
 # This IP addresses ensure debug toolbar shows development environment
 INTERNAL_IPS = ('127.0.0.1', '10.0.2.2')
 
 ALLOWED_HOSTS = ['localhost', '192.168.130.163']
 
-CELERY_RESULT_BACKEND = 'django-db'
+
+# ***** DEBUG-CORS-HEADERS SETTINGS ***** #
+# CORS_ORIGIN_WHITELIST = ()
+CORS_ORIGIN_WHITELIST = (
+    'localhost:8000',
+    'localhost:8080',
+    'localhost:9000',
+    'localhost:9090',
+)
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_METHODS = (
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+    'OPTIONS',
+)
+
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+    'time-zone',
+)
+
+# ***** CELERY SETTINGS ***** #
+# CELERY_RESULT_BACKEND = 'django-db'
 # CELERY_RESULT_BACKEND = 'django-cache'
