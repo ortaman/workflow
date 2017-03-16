369635c27a82b18fa24d11eb0d40c2e5b48f3b9e
king <kingjavanlee@hotmail.com>
2017-03-16 01:10:50 -0600

    feature:project cru data

--- spa/src/app/project-create/project-create.js
+++ spa/src/app/project-create/project-create.js
@@ -1,10 +1,14 @@
 
 app.controller('ProjectCreateController', ['$scope', function($scope) {
-  
+
   console.log('ProjectCreateController');
- 
+
    $scope.isActive = function(path) {
     return ($location.path()==path)
+  }
+
+  $scope.submit = function(data) {
+    console.log(data);
   }
 
 }]);
--- spa/src/assets/app.js
+++ spa/src/assets/app.js
@@ -292,9 +292,9 @@ app.controller('ProfileController', ['$scope', function($scope) {
 }]);
 
 
-app.controller('ProjectCreateController', ['$scope', function($scope) {
+app.controller('ProjectDetailController', ['$scope', function($scope) {
   
-  console.log('ProjectCreateController');
+  console.log('ProjectDetailController');
  
    $scope.isActive = function(path) {
     return ($location.path()==path)
@@ -303,12 +303,16 @@ app.controller('ProjectCreateController', ['$scope', function($scope) {
 }]);
 
 
-app.controller('ProjectDetailController', ['$scope', function($scope) {
-  
-  console.log('ProjectDetailController');
- 
+app.controller('ProjectCreateController', ['$scope', function($scope) {
+
+  console.log('ProjectCreateController');
+
    $scope.isActive = function(path) {
     return ($location.path()==path)
+  }
+
+  $scope.submit = function(data) {
+    console.log(data);
   }
 
 }]);
