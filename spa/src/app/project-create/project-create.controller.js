51bb4442b427cc0bf0b24d025c52ec3d146b5b14
Tenorio Orta JM <ente011@gmail.com>
2017-03-20 22:53:12 -0600

    feat: Add teh angular invalid project form validation

--- spa/src/app/project-create/project-create.controller.js
+++ spa/src/app/project-create/project-create.controller.js
@@ -5,28 +5,34 @@ app.controller('ProjectCreateController', [
     $scope.submitted = false;
     $scope.project = {};
 
-    $scope.submitForm = function (){
+    $scope.submitForm = function() {
       $scope.submitted = true;
 
-        $scope.project.preparation_at = moment($scope.project.preparation_at1).format("DD-MM-YYYY");
-      	$scope.project.negotiation_at = moment($scope.project.negotiation_at1).format("DD-MM-YYYY");
-      	$scope.project.execution_at = moment($scope.project.execution_at1).format("DD-MM-YYYY");
-      	$scope.project.evaluation_at = moment($scope.project.evaluation_at1).format("DD-MM-YYYY");
-      	$scope.project.begin_at = moment($scope.project.begin_at1).format("DD-MM-YYYY");
-      	$scope.project.accomplish_at = moment($scope.project.accomplish_at1).format("DD-MM-YYYY");
-      	$scope.project.renegotiation_at = moment($scope.project.renegotiation_at1).format("DD-MM-YYYY");
-      	$scope.project.report_at = moment($scope.project.report_at1).format("DD-MM-YYYY");
+      if ($scope.projectForm.$invalid) {
+        $scope.error = 'El formulario no es válido o no ha sido modificado.';
+        console.log($scope.projectForm);
+        return;
+      }
 
-		ProjectCreateService.create($scope.project).then(
-			function(response) {
-				console.log('reponse', response);
-				$state.go('projectList');
-			},
-			function(errorResponse) {
-				var error = errorResponse || 'Request failed';
-	    		console.log('error', error);
-	  		}
-		);
+      $scope.project.preparation_at = moment($scope.project.preparation_at1).format("DD-MM-YYYY");
+    	$scope.project.negotiation_at = moment($scope.project.negotiation_at1).format("DD-MM-YYYY");
+    	$scope.project.execution_at = moment($scope.project.execution_at1).format("DD-MM-YYYY");
+    	$scope.project.evaluation_at = moment($scope.project.evaluation_at1).format("DD-MM-YYYY");
+    	$scope.project.begin_at = moment($scope.project.begin_at1).format("DD-MM-YYYY");
+    	$scope.project.accomplish_at = moment($scope.project.accomplish_at1).format("DD-MM-YYYY");
+    	$scope.project.renegotiation_at = moment($scope.project.renegotiation_at1).format("DD-MM-YYYY");
+    	$scope.project.report_at = moment($scope.project.report_at1).format("DD-MM-YYYY");
+
+  		ProjectCreateService.create($scope.project).then(
+  			function(response) {
+  				console.log('reponse', response);
+  				$state.go('projectList');
+  			},
+  			function(errorResponse) {
+  				var error = errorResponse || 'Request failed';
+  	    		console.log('error', error);
+  	  		}
+  		);
     
     }
 
