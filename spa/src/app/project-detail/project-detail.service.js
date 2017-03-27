69034fbdf00c8da7ef34ac5229ccc0eb49953fdf
king <you@example.com>
2017-03-26 22:47:18 -0600

    feature: producers list with bootstrap

--- spa/src/app/project-detail/project-detail.service.js
+++ spa/src/app/project-detail/project-detail.service.js
@@ -1,12 +1,46 @@
 
 app.service("ProjectGetService", ['$http', 'APIConfig', function($http, APIConfig) {
-  
+
   this.getById = function(id) {
     var promise = $http.get(APIConfig.url + "projects/" + id + "/").then(function(response) {
       return response.data;
     });
 
     return promise;
-  }  
+  }
+
+}]);
+
+app.service("ProducerGetListService", ['$http', 'APIConfig', function($http, APIConfig) {
+
+  this.getList = function(object) {
+	  var params = $.param(object);
+
+	  var promise = $http.get(APIConfig.url + "projects/?" + params).then(function(response) {
+      var a = [
+        {
+          "user":"juan1",
+        },
+        {
+          "user":"juan2",
+        },
+        {
+          "user":"juan3",
+        },
+        {
+          "user":"juan4",
+        },
+        {
+          "user":"juan5",
+        },
+        {
+          "user":"juan6",
+        },
+      ];
+
+	  return a;
+	});
+	  return promise;
+	};
 
 }]);
