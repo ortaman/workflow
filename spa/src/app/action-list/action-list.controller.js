cbde5d1387b8be819e33be341affe16bb86d0f3e
king <you@example.com>
2017-04-20 16:55:17 -0500

    feature: producers performance

--- spa/src/app/action-list/action-list.controller.js
+++ spa/src/app/action-list/action-list.controller.js
@@ -1,20 +1,51 @@
 
-app.controller('ActionListController', ['$scope', 'ActionListService', function($scope, ActionListService) {
+app.controller('ActionListController', ['$scope', 'ActionListService', 'ProducerGetListService',
+ function($scope, ActionListService, ProducerGetListService) {
 
-  $scope.actions = [
-    {
-      "producer":{
-        'name':"jan",
-        "first_surname":"valdes",
-        "position":"empleado"
-      },
-      'promise':{
-        "kept_percent": 1,
-        "kept": 2,
-        "empty": 3,
-        "negotiating": 4,
-        "open": 5,
+   $scope.producersCurrentPage = 1;
+
+   $scope.init = function () {
+     //$scope.producerPageChanged();
+
+   }
+
+
+    $scope.actions = [
+      {
+        "producer":{
+          'name':"jankl",
+          "first_surname":"valdes",
+          "position":"empleado"
+        },
+        'promise':{
+          "kept_percent": 1,
+          "kept": 2,
+          "empty": 3,
+          "negotiating": 4,
+          "open": 5,
+        }
       }
-    }
-  ]
+    ]
+
+
+  	$scope.producerPageChanged = function() {
+
+  	  	var query = {
+  	  		"page": $scope.producersCurrentPage,
+  	  	};
+
+  		ProducerGetListService.getList(query).then(
+  			function(response) {
+
+          console.log("respue", response);
+
+  			},
+  			function(errorResponse) {
+  				$scope.status = errorResponse.statusText || 'Request failed';
+  				$scope.errors = errorResponse.data;
+  			}
+  		);
+
+    };
+
 }]);
