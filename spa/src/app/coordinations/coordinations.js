0953f4e91396af8b6a8b2d2959f7d972ff312502
king <you@example.com>
2017-04-24 14:29:51 -0500

    fix: user images

--- spa/src/app/coordinations/coordinations.js
+++ spa/src/app/coordinations/coordinations.js
@@ -1,6 +1,6 @@
 
-app.controller('CoordinationsController', ['$scope','ActionListService','UserService','ActionCreateService','$mdDialog',
-  function($scope, ActionListService, UserService, ActionCreateService, $mdDialog) {
+app.controller('CoordinationsController', ['$scope','ActionListService','UserService','ActionCreateService','$mdDialog','APIConfig',
+  function($scope, ActionListService, UserService, ActionCreateService, $mdDialog, APIConfig) {
 
   $scope.promises = [];
   $scope.user;
@@ -21,22 +21,28 @@ app.controller('CoordinationsController', ['$scope','ActionListService','UserSer
     };
 
     if(userType == 'producer'){
+      $scope.producerStatus = status
       query.producer = $scope.user; // id usuario
       delete(query.client);
     }
 
     else if (userType == 'client'){
+      $scope.clientStatus = status
       query.client = $scope.user; // id usuario
       delete(query.producer);
     }
 
     ActionListService.getList(query).then(
       function(response) {
-        if(userType == 'producer')
+        if(userType == 'producer'){
           $scope.promises = response.results;
-        else if ( userType == 'client')
-          $scope.orders = response.results;
+          $scope.getPhoto($scope.promises)
+        }
 
+        else if ( userType == 'client'){
+          $scope.orders = response.results;
+          $scope.getPhoto($scope.orders)
+          }
       },
       function(errorResponse) {
         $scope.status = errorResponse.statusText || 'Request failed';
@@ -45,14 +51,14 @@ app.controller('CoordinationsController', ['$scope','ActionListService','UserSer
     );
   }
 
-  $scope.makeAction = function(action,type){
+  $scope.makeAction = function(action,type, currentTab){
+    console.log(action);
     var response = confirm("¿Está seguro que quiere aceptar esta accion?");
     if(response == true){
       action.promise = type
       ActionCreateService.update(action.id,action).then(
         function (response) {
-          $scope.getCordinations('producer','Creada' );
-          $scope.getCordinations('client','Creada' );
+          $scope.getCordinations('producer',currentTab );
         },
         function (errors) {
           console.log(errors);
@@ -79,4 +85,12 @@ app.controller('CoordinationsController', ['$scope','ActionListService','UserSer
 
     });
 	}
+
+  $scope.getPhoto = function (obj) {
+    console.log("obj",obj);
+    angular.forEach(obj, function(item){
+      item.project.image =  angular.copy(APIConfig.baseUrl+ item.project.image)
+      item.producer.photo =  angular.copy(APIConfig.baseUrl+ item.producer.photo)
+    })
+  }
 }]);
