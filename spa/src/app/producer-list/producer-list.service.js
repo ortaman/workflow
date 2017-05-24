9ac5465028fe7f4fb852b25a4ebb2132403c9826
king <you@example.com>
2017-05-24 11:46:43 -0500

    fix_ Nan fixed

--- spa/src/app/producer-list/producer-list.service.js
+++ spa/src/app/producer-list/producer-list.service.js
@@ -1,5 +1,14 @@
 
 app.service("ProducerGetListService", ['$http', 'APIConfig', function($http, APIConfig) {
+
+  //get
+  var getPercentage = function (producer) {
+    var result = 0 ;
+    result = (producer.satisfactories*100)/(producer.pending+ producer.unsatisfactories+ producer.satisfactories)
+    result = isNaN(result) ? 0 : result;
+    return result;
+  }
+
   this.getList = function(object) {
     var params = $.param(object);
 
@@ -7,6 +16,7 @@ app.service("ProducerGetListService", ['$http', 'APIConfig', function($http, API
 
 			for (var i=0; i < response.data.producers.length; i++) {
 				response.data.producers[i].producer.photo = APIConfig.baseUrl + response.data.producers[i].producer.photo;
+				response.data.producers[i].succesfulPercentage = getPercentage(response.data.producers[i])
 			}
       return response.data;
     });
