04484550e9932bbfeef9885caa066cb0d45fadaf
king <you@example.com>
2017-07-12 18:16:05 -0500

    feat: end point integrated

--- spa/src/app/alerts/alerts.controller.js
+++ spa/src/app/alerts/alerts.controller.js
@@ -1,6 +1,15 @@
 
-app.controller('AlertsController', ['$scope', '$timeout',
- function($scope, $timeout) {
+app.controller('AlertsController', ['$scope', 'AlertsService', 'Notification',
+ function($scope, AlertsService, Notification) {
 
+
+   AlertsService.getList({}).then(
+     function (response) {
+       console.log(response);
+     },
+     function (error) {
+       Notification.error("Ocurrio un  error, intente mas tarde");
+     }
+   )
 
 }]);
