9235ae4d5f8bb970662f6cb851cc0b846e27f373
king <you@example.com>
2017-07-14 17:57:19 -0500

    fix: alerts style fixed

--- spa/src/app/services/alerts.js
+++ spa/src/app/services/alerts.js
@@ -1,17 +1,41 @@
 app.service("AlertsService", ['$http', 'APIConfig', function($http, APIConfig) {
 
-  var kindTraduction = {
-    'Before': 'Por terminar',
+  var actionKindText = {
+    'Before': 'Acción por terminar',
     'Deadline': 'Terminó',
-    'After': 'Retradado',
+    'After': 'Expirado',
   }
 
+  var projectKindText = {
+    'Before': 'Proyecto por terminar',
+    'Deadline': 'Terminó proyecto',
+    'After': 'Expirado',
+  }
+
+  var icons = {
+    'Before': 'close',
+    'Deadline': 'check',
+    'After': 'clock',
+  }
+
+  var getIconStatus = function (item) {
+    return icons[item.kind];
+  }
+
+  var getAlertText = function (item) {
+    if(item.action.parent_action){
+      return actionKindText[item.kind] + "("+ item.action.name+")"
+    }
+    return projectKindText[item.kind]+  "("+ item.action.name +")";
+  }
 
   this.getList = function(object) {
 	  var params = $.param(object);
-
 	  var promise = $http.get(APIConfig.url + "actions/alerts/?" + params).then(function(response) {
-
+        angular.forEach(response.data.results, function (item) {
+          item.iconStatus = getIconStatus(item);
+          item.alertText = getAlertText(item);
+        })
 	     return response.data;
 	  });
 	  return promise;
@@ -19,11 +43,9 @@ app.service("AlertsService", ['$http', 'APIConfig', function($http, APIConfig) {
 
 
   this.update = function(object) {
-
     var promise = $http.put(APIConfig.url + "actions/alerts/" + object.id + "/" , object).then(function(response) {
       return response.data;
     });
-
     return promise;
   };
 
