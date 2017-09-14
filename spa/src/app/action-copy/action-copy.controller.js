49d370bbda52e9b6171151513764a556fb30d0eb
king <you@example.com>
2017-09-14 16:14:03 -0500

    fix: client assignation in action copy

--- spa/src/app/action-copy/action-copy.controller.js
+++ spa/src/app/action-copy/action-copy.controller.js
@@ -3,12 +3,6 @@ app.controller('ActionCopyController', [
   '$scope', '$state', 'ActionService', 'ProjectService', 'UserService', 'Notification',
   function ($scope, $state, ActionService, ProjectService, UserService, Notification) {
     $scope.titles = {
-      'project': {
-        'type': 'project',
-        'create': 'Crear Proyecto',
-        'nameOf': 'Nombre del proyecto',
-        'rolesOf': 'Roles del Proyecto'
-      },
       'action': {
         'type': 'action',
         'create': 'Agregar acción a:',
@@ -37,11 +31,11 @@ app.controller('ActionCopyController', [
     }
 
     var Service = ProjectService;
-    var type = $state.params.action ? 'action' : 'project' // TODO: por definir
+    var type = $state.params.action ? 'action' : 'project';
     $scope.titles = $scope.titles[type];
     $scope.submitted = false; // TODO: cambiar  con  bandera de form
-    $scope.project = {
-    };
+    $scope.datesValidations = {};
+    $scope.project = {};
 
     $scope.init = function () {
       getProject()
@@ -83,14 +77,21 @@ app.controller('ActionCopyController', [
 
     }
 
-
-    var getProject = function () {
+    /////////////////Services///////////////////////////////
+    let getProject = function () {
       ActionService.getById($state.params.action).then(
         function (response) {
           $scope.project = response;
           Service = ActionService;
-          $scope.client = response.client.name + " " + response.client.first_surname + " " + response.client.second_surname;
-          $scope.project.client = response.client.id;
+          UserService.me().then(user => {
+            $scope.client = user.name + " " + user.first_surname + " " + user.second_surname;
+            $scope.project.client = user.id;
+          })
+          $scope.datesValidations.accomplish_at = {
+            min: (new moment($scope.project.begin_at)).toDate(),
+            max: (new moment($scope.project.accomplish_at)).toDate(),            
+          }
+
         },
         function (error) {
           Notification.error("No existe un proyecto relacionado")
@@ -98,16 +99,11 @@ app.controller('ActionCopyController', [
         }
       )
     }
+
     ////////////////////dates validations///////////////////////
 
-    $scope.beginOrAtOrAccomplishDateChanged = function (begin_at, accomplish_at) {
-      // delete $scope.project.renegotiation_at
-      // if (accomplish_at){
-      //   var minDate = moment(begin_at).add(1, 'd');
-      //   $scope.renegotiation_min_date = minDate.toDate();
-      //   var maxDate = Math.ceil(moment(accomplish_at).diff(minDate, 'days')/ 2)
-      //   $scope.renegotiation_max_date = moment(accomplish_at).subtract(maxDate, 'd').toDate();
-      // }
+    $scope.getMinDate = function (date) {
+      return new moment(date).format('DD-MM-YYYY');
     }
 
     var getMaxExecutionDate = function () {
@@ -115,13 +111,7 @@ app.controller('ActionCopyController', [
     }
 
     ////////////////////end dates validations///////////////////////
-
-    $scope.isProject = function () {
-      return $scope.titles.type == 'project';
-    }
-
     var cleanDataForPost = function (project) {
-      console.log(project);
       if (typeof (project.project) != Number)
         project.project = project.project.id;
       delete (project.created_at)
